1b9dbd4119643fd2d559f7c818bb5263359bcaaa
Brett A Wiggins <brett@criticalanalysis.com.au>
2020-09-20 20:40:29 +1000

    added menu module

---
+++ menu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "menu.h"
+
+void main_menu(void)
+{
+	printf("=============================\n");
+	printf("Welcome to the budget program.\n");
+	printf("=============================\n");
+	printf("Please select from one of the following.\n");
+	printf("1. - Create a new budget.\n");
+	printf("2. - Load bugdet from file.\n");
+	printf("3. - Add to budget.\n");
+	printf("4. - View Reports.\n");
+	printf("5. - Search budget.\n");
+	printf("6. - Save budget to disk.\n");
+	printf("7. - Quit Application.\n");
+	printf("Your selection: ");
+}
+
---
+++ menu.h
@@ -0,0 +1,7 @@
+#ifndef MENU_H
+#define MENU_H
+
+void main_menu(void);
+void new_budget_menu(void);
+
+#endif
